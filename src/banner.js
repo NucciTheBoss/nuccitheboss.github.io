8be11b7bc639e5c9f9496fd9ea040e60b32abf28
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2024-07-25 22:32:27 -0400

    feat: add scaffolding for new home page
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

---
+++ src/banner.js
@@ -0,0 +1,42 @@
+// Copyright (C) 2024 Jason C. Nucciarone
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { html } from "lit";
+import { BaseElement } from "./base";
+
+// Hero banner shows name and allegheny national forest
+// when visitors first open web page
+class HeroBanner extends BaseElement {
+    render() {
+        return html`
+            <section class="hero-bgimage" id="home">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div
+                            class="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text"
+                        >
+                            <h2 class="hero-title">~$ whois nuccitheboss</h2>
+                            <p class="hero-content">
+                                I'm an HPC engineer building the next generation
+                                of open source supercomputing machines
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        `;
+    }
+}
+customElements.define("hero-banner", HeroBanner);
---
+++ src/navbar.js
@@ -0,0 +1,75 @@
+// Copyright (C) 2024 Jason C. Nucciarone
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { html } from "lit";
+import { BaseElement } from "./base";
+
+// Provide navigation for site.
+class NavBar extends BaseElement {
+    render() {
+        return html`
+            <header>
+                <nav
+                    class="navbar navbar-expand-lg fixed-top navbarScroll"
+                    data-bs-theme="dark"
+                >
+                    <div class="container">
+                        <a class="navbar-brand" href="#">Jason C. Nucciarone</a>
+                        <button
+                            class="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div
+                            class="collapse navbar-collapse"
+                            id="navbarSupportedContent"
+                        >
+                            <ul class="navbar-nav ms-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#home">Home</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#about">About</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#services"
+                                        >Services</a
+                                    >
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#portfolio"
+                                        >Portfolio</a
+                                    >
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#contact"
+                                        >Contact</a
+                                    >
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        `;
+    }
+}
+customElements.define("nav-bar", NavBar);
